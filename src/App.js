f1ec8dbff00b3b9a27a5e1f77aafe001fcad8b3c
Victorowsky <Victorowskyy@gmail.com>
2021-01-22 14:17:20 +0100

    user left notification

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import io from "socket.io-client";
 import Cookies from "js-cookie";
 import Homepage from "./comp/Homepage";
 import { Switch, Route } from "react-router-dom";
-import Error from "./comp/SignUp/ErrorSnackbar"
+import Error from "./comp/SignUp/ErrorSnackbar";
 import React, { useEffect, useState } from "react";
 import SignUp from "./comp/SignUp/SignUp";
 import Login from "./comp/Login/Login";
-import Account from './comp/Account/Account';
-import MyParties from './comp/MyParties/MyParties';
-import ConfirmAccount from './comp/SignUp/ConfirmAccount';
-import Success from './comp/SignUp/SuccessSnackbar';
+import Account from "./comp/Account/Account";
+import MyParties from "./comp/MyParties/MyParties";
+import ConfirmAccount from "./comp/SignUp/ConfirmAccount";
+import Success from "./comp/SignUp/SuccessSnackbar";
 import StreamingPlatformComp from "./comp/StreamingPlatformComp";
-import PartyInfo from './comp/MyParties/PartyInfo';
+import PartyInfo from "./comp/MyParties/PartyInfo";
 
-// const dbURL = 'https://stormy-refuge-26952.herokuapp.com/'
+const dbURL = "https://stormy-refuge-26952.herokuapp.com/";
 
-const socket = io('localhost:3001/');
+const socket = io("localhost:3001/");
 // const socket = io(dbURL);
 
-export const DataContext = React.createContext()
-
+export const DataContext = React.createContext();
 
 function App() {
   const [userID, setUserID] = useState();
@@ -31,93 +30,112 @@ function App() {
   const [isError, setIsError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("Try again!");
 
-
   socket.on("CheckUserIDAnswer", (data) => {
-    setUserData(data)
-    setNickname(data.username)
+    setUserData(data);
+    setNickname(data.username);
   });
 
-
-
   useEffect(() => {
-      if (Cookies.get("userID")) {
-        setUserID(Cookies.get("userID"));
-        setTimeout(() => {
-          socket.emit("CheckUserID", userID);
-        }, 1);
-      } 
-  },[userID]);
-
-
-
-
-
-
-
-  const handleCreateParty = (userID, partyName, text, maxUsers, streamingPlatform) =>{
-    if(userID && partyName && text && maxUsers < 6 && streamingPlatform){
-      socket.emit('createParty', {userID, partyName, text, maxUsers,streamingPlatform})
-    }else if(!userID){
-      setIsError(true)
-      setErrorMessage('You need to login!')
-    }else if(maxUsers > 5 ){
-      setIsError(true)
-      setErrorMessage('Party can have max 5 users')
-    }else{
-     setIsError(true)
-     setErrorMessage('Check it one more time')
+    if (Cookies.get("userID")) {
+      setUserID(Cookies.get("userID"));
+      setTimeout(() => {
+        socket.emit("CheckUserID", userID);
+      }, 1);
     }
-  }
+  }, [userID]);
 
+  const handleCreateParty = (
+    userID,
+    partyName,
+    text,
+    maxUsers,
+    streamingPlatform
+  ) => {
+    if (userID && partyName && text && maxUsers < 6 && streamingPlatform) {
+      socket.emit("createParty", {
+        userID,
+        partyName,
+        text,
+        maxUsers,
+        streamingPlatform,
+      });
+    } else if (!userID) {
+      setIsError(true);
+      setErrorMessage("You need to login!");
+    } else if (maxUsers > 5) {
+      setIsError(true);
+      setErrorMessage("Party can have max 5 users");
+    } else {
+      setIsError(true);
+      setErrorMessage("Check it one more time");
+    }
+  };
 
   return (
     <>
-    <DataContext.Provider 
-    value={{userID, setUserID,nickname, setNickname, userData, setUserData, socket, isSuccess, setIsSuccess, successMessage, setSuccessMessage, isError, setIsError, errorMessage,setErrorMessage, handleCreateParty}}>
-
-    <div className="app">
-    <Switch>
-      <Route path="/" exact>
-        <Homepage/>
-      </Route>
-      <Route path="/signup" exact>
-        <SignUp />
-      </Route>
-      <Route path="/login">
-        <Login/>
-        </Route>
-        <Route path="/account">
-       {userData && <Account/>} 
-        </Route>
-      <Route path="/activate">
-        <ConfirmAccount />
-      </Route>
-      <Route path="/myparties">
-       {userData && <MyParties/>}
-      </Route>
-      <Route path='/Netflix'>
-          <StreamingPlatformComp color="#e50914" streamingPlatform="Netflix"/>
-      </Route>
-      <Route path='/Spotify'>
-          <StreamingPlatformComp color="#1DB954" streamingPlatform="Spotify"/>
-      </Route>
-      <Route path='/HBOGO'>
-          <StreamingPlatformComp color="white" streamingPlatform="HBOGO"/>
-      </Route>
-      <Route path='/Disney+'>
-          <StreamingPlatformComp color="#113CCF" streamingPlatform="Disney+"/>
-      </Route>
-      <Route path='/party/:partyID' component={PartyInfo}/>
-
-
-    </Switch>
-  </div>
-      <Success/>
-      <Error
-      />
-
-    </DataContext.Provider>
-
+      <DataContext.Provider
+        value={{
+          userID,
+          setUserID,
+          nickname,
+          setNickname,
+          userData,
+          setUserData,
+          socket,
+          isSuccess,
+          setIsSuccess,
+          successMessage,
+          setSuccessMessage,
+          isError,
+          setIsError,
+          errorMessage,
+          setErrorMessage,
+          handleCreateParty,
+        }}
+      >
+        <div className="app">
+          <Switch>
+            <Route path="/" exact>
+              <Homepage />
+            </Route>
+            <Route path="/signup" exact>
+              <SignUp />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/account">{userData && <Account />}</Route>
+            <Route path="/activate">
+              <ConfirmAccount />
+            </Route>
+            <Route path="/myparties">{userData && <MyParties />}</Route>
+            <Route path="/Netflix">
+              <StreamingPlatformComp
+                color="#e50914"
+                streamingPlatform="Netflix"
+              />
+            </Route>
+            <Route path="/Spotify">
+              <StreamingPlatformComp
+                color="#1DB954"
+                streamingPlatform="Spotify"
+              />
+            </Route>
+            <Route path="/HBOGO">
+              <StreamingPlatformComp color="white" streamingPlatform="HBOGO" />
+            </Route>
+            <Route path="/Disney+">
+              <StreamingPlatformComp
+                color="#113CCF"
+                streamingPlatform="Disney+"
+              />
+            </Route>
+            <Route path="/party/:partyID" component={PartyInfo} />
+          </Switch>
+        </div>
+        <Success />
+        <Error />
+      </DataContext.Provider>
     </>
   );
 }
